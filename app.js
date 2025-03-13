789c1083586e47d398d0813be6c874fb2115d411
blendagsantana <blendagsantana@gmail.com>
2025-03-13 10:53:38 -0300

    Update _ Lista de Amigos app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos=[];
+function adicionarAmigos(){
+  let amigoCompara=document.getElementById("amigo").value;
+  if (amigoCompara===""){
+    console.log("Por favor, insira um nome.")}
+    else{ amigos.push(amigoCompara)
+    document.getElementById("amigo").value = ""}
+}
